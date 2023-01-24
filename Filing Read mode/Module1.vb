613c3325c378104f865dbc4af58125bded157197
Taha Anwar <123452083+tahaanwar31@users.noreply.github.com>
2023-01-24 16:02:07 +0500

    Read mode for filling

---
+++ Filing Read mode/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim name As String
+        name = ""
+        FileOpen(1, My.Application.Info.DirectoryPath & "\BCCPASStudents.txt", OpenMode.Input)
+        While Not EOF(1)
+            Input(1, name)
+            Console.WriteLine(name)
+        End While
+        FileClose(1)
+        Console.ReadKey()
+    End Sub
+
+End Module
